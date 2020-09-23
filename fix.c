fc72099ebcf236b3ff6fad5a6d293eed89ae0580
dzikrula <dzikrulakbar4@gmail.com>
2020-09-23 19:59:03 +0700

    sae

--- fix.c
+++ fix.c
@@ -16,7 +16,7 @@ void pilihan()
 void Input()
 { 
     int i,j;
-    printf("\n===============hmmmmm===============\n");
+    printf("\n===============Matriks A===============\n");
     for (i=0; i<2; i++){
         for (j=0; j<2; j++){
             printf("Input Matrix A[%d][%d] : ",i, j);
